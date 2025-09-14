edb8f00e668e2bbe15e7f7e7c49c06b0a9fff1b3
abhinavA4 <abhinav.dev@yahoo.com>
2025-09-14 23:30:19 +0530

    Update 0.2.1

--- src/components/Cardd.jsx
+++ src/components/Cardd.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
-export const Cardd = ({nameTag="ARTWORK-99" , priceTag="$99" , imgTag = "pro-1.jpg" , altTag = "Flag"}) => {
+export const Cardd = ({nameTag="ARTWORK-99" , imgTag = "pro-1.jpg" , altTag = "Flag"}) => {
     return (
         <div>
             <div>
-                <img src={imgTag} alt={altTag} loading="lazy" className='w-full h-60 object-cover pb-2'/>
-                <h3>{nameTag}</h3>
-                <h3>{priceTag}</h3>
+                <img src={`${nameTag}.svg`} alt={nameTag} loading="lazy" className='w-full h-60 object-cover border-2'/>
+                <h1 className='text-center text-xl uppercase cursor-pointer hover:text-[#6611f5]'>{nameTag}</h1>
             </div>
-        </div>
+        </div> 
     )
 }
